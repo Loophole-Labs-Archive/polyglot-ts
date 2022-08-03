9b40038024d6fb655af7095bfc3cc6230bad13fc
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-08-03 17:18:58 +0200

    feat: Add simple AST and namespace parser

---
+++ src/ast.test.ts
@@ -0,0 +1,78 @@
+/*
+	Copyright 2022 Loophole Labs
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+import protobuf from "protobufjs";
+import path from "path";
+import { getRootAndNamespace, getTypes } from "./ast";
+
+describe("AST", () => {
+  it("Can get root and namespace from simple protobuf", () => {
+    const proto = protobuf
+      .loadSync(path.join(__dirname, "..", "data", "simple.proto"))
+      .toJSON().nested!;
+
+    const { root, namespace } = getRootAndNamespace(proto);
+
+    expect(namespace).toEqual([
+      "io",
+      "loopholelabs",
+      "polyglot",
+      "test",
+      "data",
+    ]);
+    expect(root).toMatchSnapshot();
+  });
+
+  it("Can get AST from simple protobuf", () => {
+    const proto = protobuf
+      .loadSync(path.join(__dirname, "..", "data", "simple.proto"))
+      .toJSON().nested!;
+
+    const { root } = getRootAndNamespace(proto);
+    const types = getTypes(root);
+
+    expect(types).toMatchSnapshot();
+  });
+
+  it("Can get root and namespace from complex, unordered protobuf", () => {
+    const proto = protobuf
+      .loadSync(path.join(__dirname, "..", "data", "complex.proto"))
+      .toJSON().nested!;
+
+    const { root, namespace } = getRootAndNamespace(proto);
+
+    expect(namespace).toEqual([
+      "io",
+      "loopholelabs",
+      "polyglot",
+      "test",
+      "data",
+      "complex",
+    ]);
+    expect(root).toMatchSnapshot();
+  });
+
+  it("Can get AST from complex, unordered protobuf", () => {
+    const proto = protobuf
+      .loadSync(path.join(__dirname, "..", "data", "complex.proto"))
+      .toJSON().nested!;
+
+    const { root } = getRootAndNamespace(proto);
+    const types = getTypes(root);
+
+    expect(types).toMatchSnapshot();
+  });
+});
---
+++ src/ast.ts
@@ -0,0 +1,80 @@
+/*
+	Copyright 2022 Loophole Labs
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+interface ISrcTypes {
+  [k: string]: protobuf.AnyNestedObject;
+}
+
+interface ISrcType {
+  fields: {
+    [k: string]: ISrcField;
+  };
+}
+
+interface ISrcField {
+  type: string;
+  id: number;
+}
+
+interface IDstType {
+  typeName: string;
+  fields: IDstField[];
+}
+
+interface IDstField {
+  fieldName: string;
+  typeName: string;
+}
+
+export const getTypes = (root: ISrcTypes): IDstType[] =>
+  Object.keys(root).map((typeName) => {
+    const srcTypeFields = (root[typeName] as ISrcType).fields;
+
+    return {
+      typeName,
+      fields: Object.keys(srcTypeFields)
+        .map((fieldName) => ({
+          fieldName,
+          typeName: srcTypeFields[fieldName].type,
+          index: srcTypeFields[fieldName].id,
+        }))
+        .sort((curr, prev) => curr.index - prev.index)
+        .map((field) => ({
+          fieldName: field.fieldName,
+          typeName: field.typeName,
+        })),
+    };
+  });
+
+export const getRootAndNamespace = (
+  obj: any,
+  objPath: string[] = [],
+  lastObj: any = undefined
+): { root: any; namespace: string[] } => {
+  if (obj.fields) {
+    return {
+      root: lastObj,
+      namespace: objPath.slice(0, -1),
+    };
+  }
+
+  const nextKey = Object.keys(obj)[0];
+  if (nextKey === "nested") {
+    return getRootAndNamespace(obj[nextKey], objPath, obj);
+  }
+
+  return getRootAndNamespace(obj[nextKey], [...objPath, nextKey], obj);
+};
